1b6b7d12f277567412532299d4c38130c2bcdb69
veliciagabriell <19623135@std.stei.itb.ac.id>
2024-11-19 22:32:39 +0700

    feat: add storelist, storerequest, storeremove

---
+++ src/command/store.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include "store.h"
+#include "../ADT/mesinkata.h"
+#include "../ADT/mesinkarakter.h"
+
+void storelist () {
+   
+    printf(" _____ _____ ___________ _____ _     _____ _____ _____ \n");
+    printf("/  ___|_   _|  _  | ___ \\  ___| |   |_   _/  ___|_   _|\n");
+    printf("\\ `--.  | | | | | | |_/ / |__ | |     | | '\\ `--.  | | \n");
+    printf(" `--. \\ | | | | | |    /|  __|| |     | |  `--. \\ | |  \n");
+    printf("/\\_/ / | | \\ \\_/ / |\\ \\| |___| |_____| |_/\\__/ / | |  \n");
+    printf("\\____/  \\_/  \\___/\\_| \\_\\____/\\_____/\\___/\\____/  \\_/\n");
+
+
+    printf("List barang yang ada di toko: \n");
+    
+    STARTWORDFILE("config.txt");
+    // if (IsEmpty(itemList)) {
+    //     printf("TOKO KOSONG");
+    // }
+    // else { 
+        printf("- ");
+        printf("%s", currentWord);
+    // }   
+    printf("\n");
+    // }
+}
+
+
+    item itemList[] = {
+    {20, "AK47"},
+    {20, "Lalabu"},
+    {10, "Ayam Goreng Crisbar"}
+    };
+
+    int itemCount = sizeof(itemList) / sizeof(itemList[0]);
+    
+void request (List *L, Queue *Q) {
+    char req;
+    char val;
+    boolean found = 0;
+
+    printf("Nama barang yang diminta: ");
+    scanWord(&req);
+    printf("\n");
+
+    for (int i = 0; i < itemCount; i++) {
+        if (isEmpty(*Q) && Search(*L,req)) {
+            printf("Barang dengan nama yang sama sudah ada di toko\n");
+        }
+        else if (isEmpty(*Q) && !Search(*L,req)) {
+            enqueue(Q, req);
+        }
+        else if (!isEmpty(*Q) && !Search(*L,req)) {
+            while(!isEmpty(*Q) && !found) {
+                dequeue(Q, &val);
+                if (val == req) {
+                    enqueue(Q, val);
+                    found = 1;
+                }
+                else {
+                    enqueue(Q, val);
+                }
+                
+            }
+
+            if (found) {
+                printf("Barang dengan nama yang sama sudah ada di antrian\n");
+            }
+            else {
+                enqueue(Q, req);
+            }
+        }
+    }
+}
+
+void remove(List L) {
+    char item_name[100];
+    printf("Nama barang yang akan dihapus: ");
+    scanWord(&item_name);
+
+    
+    boolean found = 0;
+    int i = 0;
+
+    while (!IsEmpty(L) || found) {
+        if (item_name == L.A[i]) {
+            found = 1;
+        }
+        else {
+            found = 0;
+        }
+    }
+
+    if (found) {
+        printf("%s telah berhasil dihapus.\n", item_name);
+    }
+    else {
+        pritnf("Toko tidak menjual %s.", item_name);
+    }
+}
+
+int main() {
+    // List itemList[] = {
+    // {20, "AK47"},
+    // {20, "Lalabu"},
+    // {10, "Ayam Goreng Crisbar"}
+    // };
+
+    storelist();
+    return 0;
+}
---
+++ src/command/store.h
@@ -0,0 +1,25 @@
+#ifndef storelist_H
+#define storelist_H
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include "../ADT/list.h"
+#include "../ADT/queue.h"
+#include "../ADT/mesinkata.h"
+#include "../ADT/mesinkarakter.h"
+
+// typedef struct {
+//     int price;
+//     char name[100];
+// } List;
+
+
+void storelist ();
+// I.S List bisa saja kosong
+// F.S Jika list kosong, mencetak "TOKO KOSONG"
+// Jika list tidak kosong, mencetak seluruh barang yang ada di toko
+
+// void remove(List L);
+
+// void request (List *L, Queue *Q);
+#endif
