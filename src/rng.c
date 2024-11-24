b44c597dab79dfc14c2d05e56f5876c9b661d733
unknown <19623037@std.stei.itb.ac.id>
2024-11-24 20:34:06 +0700

    fix: rng with money implementation

--- src/rng.c
+++ src/rng.c
@@ -4,38 +4,47 @@
 #include "ADT/mesinkata.h"
 #include "misc.h"
 
-void tebakAngkaRNG() {
-    const int MIN = 1;
-    const int MAX = 100;
+void tebakAngkaRNG(int *money) {
     int guess, answer, chance = 10, initial_reward = 500, reward;
+    int min = 1;
+    int max = 100;
+    int cost = 200;
     Word inputWord;
 
+    // cek duit
+    if (*money < cost) {
+        printf("Uang anda kurang, permainan ini membutuhkan 200 koin, koin anda saat ini %d\n", *money);
+        return;
+    }
+
+    // kurangi duit
+    *money -= cost;
+
     srand(time(0));
-    answer = (rand() % MAX) + MIN;
+    answer = (rand() % max) + min;
     printf("%d<<<<JAWABAN\n", answer); // sementara di liatin buat testing aje
 
     for (int i = 1; i <= chance; i++) {
         printf("Tebak angka: ");
-        // scanf("%d", &guess);
         scanWord(&inputWord);
 
-        // Convert word to int
-        if(isKataInt(inputWord)){
+        // convert
+        if (isKataInt(inputWord)) {
             guess = convertWordToInt(inputWord);
-        }
-        else {
+        } else {
             printf("Input tidak valid. Tolong masukkan angka!\n");
             i--;
             continue;
         }
-        
+
         if (guess == answer) {
             reward = initial_reward - ((i - 1) * (initial_reward / chance));
             if (reward < 50) {
                 reward = 50;
             }
             printf("Tebakanmu benar!, +%d rupiah telah ditambahkan ke akun anda.\n", reward);
-            return; 
+            *money += reward; // tambah duit
+            return;
         } else if (guess > answer) {
             printf("Tebakanmu lebih besar!\n");
         } else if (guess < answer) {
@@ -45,7 +54,10 @@ void tebakAngkaRNG() {
     printf("Kesempatan habis! Angka yang benar adalah %d.\n", answer);
 }
 
-/*int main(){
-    tebakAngkaRNG();
+
+/*int main() {
+    int money = 1000; 
+    tebakAngkaRNG(&money);
+    printf("Sisa uang anda: %d\n", money);
     return 0;
 }*/
