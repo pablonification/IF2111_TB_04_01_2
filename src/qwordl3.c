d4af63281b5acd957942e06cad20de8e10dd5848
OmarBerliansyah <19623295@std.stei.itb.ac.id>
2024-11-24 19:32:52 +0700

    revisi qwordl3 dan wordl32

--- src/qwordl3.c
+++ src/qwordl3.c
@@ -52,60 +52,113 @@ boolean isValidGuessq(Word guess) {
 boolean compareWordss(const char *word1, const char *word2) {
     for (int i = 0; i < WORD_LENGTH; i++) {
         if (word1[i] != word2[i]) {
-            return FALSE; // Words are not equal
+            return FALSE;
         }
     }
-    return TRUE; // Words are equal
+    return TRUE; 
 }
 
 void INITIALIZED_QWORDL3(char ***wordsList, int *wordCount) {
     LoadWordsFromFile("words.txt", wordsList, wordCount);
 }
 
-void playQuantumWordl3() {
-    char **wordsList;
-    int wordCount;
-    INITIALIZED_QWORDL3(&wordsList, &wordCount);
+void playQuantumWordl3(int money) {
+    if (money < 1000){
+        printf("Uang anda kurang, challange ini membutuhkan 500 koin, koin anda saat ini %d\n", money);
+        return;
+    } else {
+        money -= 1000;
+        char **wordsList;
+        int wordCount;
+        INITIALIZED_QWORDL3(&wordsList, &wordCount);
 
 
-    // Pick four unique target words
-    srand(time(NULL));
-    char *answers[TOTAL_WORDS];
-    for (int i = 0; i < TOTAL_WORDS; i++) {
-        boolean isUnique;
-        do {
-            isUnique = TRUE;
-            answers[i] = wordsList[rand() % wordCount];
-            for (int j = 0; j < i; j++) {
-                if (compareWordss(answers[i], answers[j])) {
-                    isUnique = FALSE;
-                    break;
+        // Pick four unique target words
+        srand(time(NULL));
+        char *answers[TOTAL_WORDS];
+        for (int i = 0; i < TOTAL_WORDS; i++) {
+            boolean isUnique;
+            do {
+                isUnique = TRUE;
+                answers[i] = wordsList[rand() % wordCount];
+                for (int j = 0; j < i; j++) {
+                    if (compareWordss(answers[i], answers[j])) {
+                        isUnique = FALSE;
+                        break;
+                    }
+                }
+            } while (!isUnique);
+        }
+
+        // Initialize game board
+        char boardq[TOTAL_WORDS][MAX_ATTEMPTSQ][WORD_LENGTH * 3 + 1];
+        for (int w = 0; w < TOTAL_WORDS; w++) {
+            for (int i = 0; i < MAX_ATTEMPTSQ; i++) {
+                for (int j = 0; j < WORD_LENGTH * 3; j += 3) {
+                    boardq[w][i][j] = '_';
+                    boardq[w][i][j + 1] = ' ';
+                    boardq[w][i][j + 2] = ' ';
+                }
+                boardq[w][i][WORD_LENGTH * 3] = '\0'; // Null-terminate each row
+            }
+        }
+
+        int attempts = 0;
+        boolean win = FALSE;
+        Word guesses[TOTAL_WORDS];
+
+        printf("WELCOME TO QUANTUM W0RDL3! GUESS 4 WORDS SIMULTANEOUSLY. YOU HAVE %d CHANCES.\n", MAX_ATTEMPTSQ);
+
+        while (attempts < MAX_ATTEMPTSQ && !win) {
+            // Display the game board
+            for (int w = 0; w < TOTAL_WORDS; w++) {
+                printf("Word %d:\n", w + 1);
+                for (int i = 0; i < MAX_ATTEMPTSQ; i++) {
+                    printf("%s\n", boardq[w][i]);
                 }
             }
-        } while (!isUnique);
-    }
 
-    // Initialize game board
-    char boardq[TOTAL_WORDS][MAX_ATTEMPTSQ][WORD_LENGTH * 3 + 1];
-    for (int w = 0; w < TOTAL_WORDS; w++) {
-        for (int i = 0; i < MAX_ATTEMPTSQ; i++) {
-            for (int j = 0; j < WORD_LENGTH * 3; j += 3) {
-                boardq[w][i][j] = '_';
-                boardq[w][i][j + 1] = ' ';
-                boardq[w][i][j + 2] = ' ';
+            for (int i = 0; i <= 3; i++){
+                printf("ANSWERS>>>>>>%s\n", answers[i]);
             }
-            boardq[w][i][WORD_LENGTH * 3] = '\0'; // Null-terminate each row
+
+            // Prompt the user for guesses
+            for (int w = 0; w < TOTAL_WORDS; w++) {
+                printf("Masukkan kata tebakan Anda untuk Word %d: ", w + 1);
+                do {
+                    STARTLINE();
+                    guesses[w] = currentWord;
+
+                    if (guesses[w].Length != WORD_LENGTH) {
+                        printf("Invalid input! Please enter a %d-letter word: ", WORD_LENGTH);
+                    }
+                } while (guesses[w].Length != WORD_LENGTH);
+            }
+
+            // Evaluate the guesses and update the boards
+            boolean allCorrect = TRUE;
+            for (int w = 0; w < TOTAL_WORDS; w++) {
+                char result[WORD_LENGTH * 3 + 1] = {0}; // Clear the result buffer
+                evaluateGuessq(answers[w], guesses[w], result);
+                for (int i = 0; i < WORD_LENGTH * 3 + 1; i++) {
+                    boardq[w][attempts][i] = result[i];
+                }
+
+                // Check if the word is correctly guessed
+                if (!compareWordss(answers[w], guesses[w].TabWord)) {
+                    allCorrect = FALSE;
+                }
+            }
+
+            // Check if all words are guessed correctly
+            if (allCorrect) {
+                win = TRUE;
+            }
+
+            attempts++;
         }
-    }
 
-    int attempts = 0;
-    boolean win = FALSE;
-    Word guesses[TOTAL_WORDS];
-
-    printf("WELCOME TO QUANTUM W0RDL3! GUESS 4 WORDS SIMULTANEOUSLY. YOU HAVE %d CHANCES.\n", MAX_ATTEMPTSQ);
-
-    while (attempts < MAX_ATTEMPTSQ && !win) {
-        // Display the game board
+        // Final display of the game board
         for (int w = 0; w < TOTAL_WORDS; w++) {
             printf("Word %d:\n", w + 1);
             for (int i = 0; i < MAX_ATTEMPTSQ; i++) {
@@ -113,64 +166,17 @@ void playQuantumWordl3() {
             }
         }
 
-        for (int i = 0; i <= 3; i++){
-            printf("ANSWERS>>>>>>%s\n", answers[i]);
-        }
-
-        // Prompt the user for guesses
-        for (int w = 0; w < TOTAL_WORDS; w++) {
-            printf("Masukkan kata tebakan Anda untuk Word %d: ", w + 1);
-            do {
-                STARTLINE();
-                guesses[w] = currentWord;
-
-                if (guesses[w].Length != WORD_LENGTH) {
-                    printf("Invalid input! Please enter a %d-letter word: ", WORD_LENGTH);
-                }
-            } while (guesses[w].Length != WORD_LENGTH);
-        }
-
-        // Evaluate the guesses and update the boards
-        boolean allCorrect = TRUE;
-        for (int w = 0; w < TOTAL_WORDS; w++) {
-            char result[WORD_LENGTH * 3 + 1] = {0}; // Clear the result buffer
-            evaluateGuessq(answers[w], guesses[w], result);
-            for (int i = 0; i < WORD_LENGTH * 3 + 1; i++) {
-                boardq[w][attempts][i] = result[i];
-            }
-
-            // Check if the word is correctly guessed
-            if (!compareWordss(answers[w], guesses[w].TabWord)) {
-                allCorrect = FALSE;
+        // Display the end message
+        if (win) {
+            printf("Selamat, Anda menang! +5000 rupiah telah ditambahkan ke akun Anda.\n");
+            money += 5000;
+        } else {
+            printf("Boo! Anda kalah. Jawaban yang benar adalah:\n");
+            for (int w = 0; w < TOTAL_WORDS; w++) {
+                printf("Word %d: %s\n", w + 1, answers[w]);
             }
         }
 
-        // Check if all words are guessed correctly
-        if (allCorrect) {
-            win = TRUE;
-        }
-
-        attempts++;
+        free(wordsList);
     }
-
-    // Final display of the game board
-    for (int w = 0; w < TOTAL_WORDS; w++) {
-        printf("Word %d:\n", w + 1);
-        for (int i = 0; i < MAX_ATTEMPTSQ; i++) {
-            printf("%s\n", boardq[w][i]);
-        }
-    }
-
-    // Display the end message
-    if (win) {
-        printf("Selamat, Anda menang! +10000 rupiah telah ditambahkan ke akun Anda.\n");
-    } else {
-        printf("Boo! Anda kalah. Jawaban yang benar adalah:\n");
-        for (int w = 0; w < TOTAL_WORDS; w++) {
-            printf("Word %d: %s\n", w + 1, answers[w]);
-        }
-    }
-
-    free(wordsList);
 }
-
--- src/wordl32.c
+++ src/wordl32.c
@@ -30,7 +30,6 @@ void evaluateGuess(const char *target, Word guess, char *result) {
         }
     }
 
-    // Second pass: Mark wrong-position matches
     for (int i = 0; i < WORD_LENGTH; i++) {
         if (result[i * 3 + 1] == '%') { 
             for (int j = 0; j < WORD_LENGTH; j++) {
@@ -63,77 +62,75 @@ void INITIALIZED_WORDL3(char ***wordsList, int *wordCount) {
     LoadWordsFromFile("words.txt", wordsList, wordCount);
 }
 
-void playWordl3() {
-    char **wordsList;
-    int wordCount;
-    INITIALIZED_WORDL3(&wordsList, &wordCount);
+void playWordl3(int money) {
+    if (money < 500){
+        printf("Uang anda kurang, challange ini membutuhkan 500 koin, koin anda saat ini %d\n", money);
+        return;
+    } else {
+        money -= 500;
+        char **wordsList;
+        int wordCount;
+        INITIALIZED_WORDL3(&wordsList, &wordCount);
 
-    // Pick a random answer
-    srand(time(NULL));
-    char *answer = wordsList[rand() % wordCount];
+        srand(time(NULL));
+        char *answer = wordsList[rand() % wordCount];
 
-    // Initialize game board
-    char board[MAX_ATTEMPTS][WORD_LENGTH * 3 + 1];
-    for (int i = 0; i < MAX_ATTEMPTS; i++) {
-        for (int j = 0; j < WORD_LENGTH * 3; j += 3) {
-            board[i][j] = '_';
-            board[i][j + 1] = ' ';
-            board[i][j + 2] = ' ';
+        char board[MAX_ATTEMPTS][WORD_LENGTH * 3 + 1];
+        for (int i = 0; i < MAX_ATTEMPTS; i++) {
+            for (int j = 0; j < WORD_LENGTH * 3; j += 3) {
+                board[i][j] = '_';
+                board[i][j + 1] = ' ';
+                board[i][j + 2] = ' ';
+            }
+            board[i][WORD_LENGTH * 3] = '\0'; 
         }
-        board[i][WORD_LENGTH * 3] = '\0'; 
-    }
 
-    int attempts = 0;
-    boolean win = FALSE;
-    Word guess;
+        int attempts = 0;
+        boolean win = FALSE;
+        Word guess;
 
-    // Display the welcome message
-    printf("WELCOME TO W0RDL3, YOU HAVE %d CHANCES TO ANSWER BEFORE YOU LOSE!\n", MAX_ATTEMPTS);
+        printf("WELCOME TO W0RDL3, YOU HAVE %d CHANCES TO ANSWER BEFORE YOU LOSE!\n", MAX_ATTEMPTS);
 
-    while (attempts < MAX_ATTEMPTS && !win) {
-        // Display the game board up to the current attempt
+        while (attempts < MAX_ATTEMPTS && !win) {
+            for (int i = 0; i < MAX_ATTEMPTS; i++) {
+                printf("%s\n", board[i]);
+            }
+        printf("ANSWER>>>>>>>>>>%s\n", answer);
+            printf("Masukkan kata tebakan Anda: ");
+            do {
+                STARTLINE();
+                guess = currentWord;
+
+
+                if (guess.Length != WORD_LENGTH) {
+                    printf("Invalid input! Please enter a %d-letter word: ", WORD_LENGTH);
+                }
+            } while (guess.Length != WORD_LENGTH);
+
+            char result[WORD_LENGTH * 3 + 1] = {0}; 
+            evaluateGuess(answer, guess, result);
+            for (int i = 0; i < WORD_LENGTH * 3 + 1; i++) {
+                board[attempts][i] = result[i];
+            }
+
+            // Check for win condition
+            if (compareWords(answer, guess, WORD_LENGTH)) {
+                win = TRUE;
+            }
+            attempts++;
+        }
+
         for (int i = 0; i < MAX_ATTEMPTS; i++) {
             printf("%s\n", board[i]);
         }
-    printf("ANSWER>>>>>>>>>>%s\n", answer);
-        // Prompt the user for input
-        printf("Masukkan kata tebakan Anda: ");
-        do {
-            STARTLINE();
-            guess = currentWord;
 
-
-            if (guess.Length != WORD_LENGTH) {
-                printf("Invalid input! Please enter a %d-letter word: ", WORD_LENGTH);
-            }
-        } while (guess.Length != WORD_LENGTH);
-
-        // Evaluate the guess and update the board for the current attempt
-        char result[WORD_LENGTH * 3 + 1] = {0}; // Clear the result buffer
-        evaluateGuess(answer, guess, result);
-        for (int i = 0; i < WORD_LENGTH * 3 + 1; i++) {
-            board[attempts][i] = result[i];
+        if (win) {
+            printf("Selamat, Anda menang!\n+1500 rupiah telah ditambahkan ke akun Anda.\n");
+            money += 1500;
+        } else {
+            printf("Boo! Anda kalah. Jawaban yang benar adalah: %s\n", answer);
         }
 
-        // Check for win condition
-        if (compareWords(answer, guess, WORD_LENGTH)) {
-            win = TRUE;
-        }
-        attempts++;
+        free(wordsList);
     }
-
-    // Final display of the game board
-    for (int i = 0; i < MAX_ATTEMPTS; i++) {
-        printf("%s\n", board[i]);
-    }
-
-    // Display the end message
-    if (win) {
-        printf("Selamat, Anda menang!\n+1500 rupiah telah ditambahkan ke akun Anda.\n");
-    } else {
-        printf("Boo! Anda kalah. Jawaban yang benar adalah: %s\n", answer);
-    }
-
-    free(wordsList);
 }
-
